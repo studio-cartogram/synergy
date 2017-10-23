086d521a9a74e02e3e0534d0467b357fe2484246
feeohnah <fiona@studiocartogram.com>
2017-10-23 19:31:59 -0400

    first stab at annoucement

--- wp-content/themes/synergy/partials/contact-announcement.php
+++ wp-content/themes/synergy/partials/contact-announcement.php
@@ -7,6 +7,13 @@ if( $announcement ):
   echo '<section class="announcement">';
 
     echo '<p>' . $announcement . '</p>';
+
+    echo '<a href="#" class="button button--close">';
+
+      set_query_var( 'icon', 'close' );
+      get_template_part('partials/icon');
+
+    echo '</a>';
   
   echo '</section>';
 
