5556a3c014fc18e16c458e05d0d034f3bc977f4c
feeohnah <fiona@studiocartogram.com>
2017-10-25 17:55:59 -0400

    initial pass at banners

--- wp-content/themes/synergy/partials/section-banner.php
+++ wp-content/themes/synergy/partials/section-banner.php
@@ -1,5 +1,8 @@
 <?php
 
+$outcomes = get_field('outcomes', 'options');
+$button_text = get_field('outcomes', 'options');
+
 $title = get_sub_field('title');
 $description = get_sub_field('description');
 $subtitle = get_sub_field('subtitle');
@@ -9,50 +12,51 @@ $image = get_sub_field('image');
 $size = 'full';
 $orientation = get_sub_field('orientation');
 
-echo '<section class="banner">';
+echo '<section class="banner banner__'. $orientation .'">';
 
   echo '<div class="banner__background">';
 
-    set_query_var('icon', 'splotches');
+    set_query_var('icon', 'halftone-arrow');
+    set_query_var('icon', 'halftone-peak');
+    set_query_var('icon', 'halftone-shift');
+    set_query_var('icon', 'halftone-square');
+    set_query_var('icon', 'halftone-swoosh');
+    set_query_var('icon', 'halftone-trio');
 
     get_template_part('partials/icon');
 
   echo '</div>';
 
-  echo '<div class="row row--justify-center">';
+  echo '<div class="banner__text">';
 
-    echo '<div class="column column-8-laptop intro">';
+    echo '<h1 class="">' . $title . ' </h1>';
 
-      echo '<h1 class="">' . $title . '</h1>';
+    echo '<h2 class="secondary">' . $subtitle . '</h2>';
 
-      echo '<h2 class="secondary">' . $subtitle . '</h2>';
+    if ($description) :
 
-      if ($description) :
+    echo '<p class="p--large">' . $description . '</p>';
 
-      echo '<p class="p--large">' . $description . '</p>';
+    endif;
 
-      endif;
+    if ($link && $link_text) :
 
-      if ($link && $link_text) :
+    echo '<a href="' . $link .'" class="link link--primary link--underline link--large">' . $link_text . '</a>';
 
-      echo '<a href="' . $link .'" class="link link--primary link--large">' . $link_text . '</a>';
+    endif;
 
-      endif;
-
-    echo '</div>';
-
-   echo '</div>';
+  echo '</div>';
 
   if( $image ) :
+    
+    echo '<div class="banner__image banner__image-crop">';
 
-    echo '<div class="content__image">';
-
-    echo wp_get_attachment_image( $image, $size );
+      echo wp_get_attachment_image( $image, $size );
 
     echo '</div>';
 
   endif;
-
+  
 echo '</section>';
 
 get_template_part('partials/section-note');
