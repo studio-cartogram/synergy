104545f0ae4b1a2e22a5a43f5cfd2e0f9923bc1a
feeohnah <fiona@studiocartogram.com>
2017-10-31 23:01:45 -0400

    first pass on mobile nav

--- wp-content/themes/synergy-dev/theme/header.php
+++ wp-content/themes/synergy-dev/theme/header.php
@@ -19,56 +19,59 @@ get_template_part('partials/contact-announcement');
 
 echo '<header role="banner" class="js-header header">';
 
-  echo '<div class="row row--full row--justify-space-between">';
+  echo '<a href="#menu" data-toggle=".js-nav" data-toggle-class="is-active" class="nav__toggle">Menu<span class="ico-caret-down right" aria-hidden="true"></span>
+  </a>';
 
-    echo '<div class="header__group header__group--left">';
+  echo '<nav class="nav__wrapper js-nav" role="navigation">';
 
-      echo '<a href="' . get_bloginfo('url') . '" class="header__logo logo">';
+    echo '<div class="row row--full nav__inner">';
 
-        echo '<span class="visuallyhidden">' . get_bloginfo('name') . '</span>';
+      echo '<div class="header__group header__group--right">';
+      
+        echo '<ul class="header__nav header__nav--right list">';
 
-        echo '<span class="logo">';
+          echo '<li class="">';
 
-        echo '<img src="' . get_bloginfo('stylesheet_directory') . '/assets/svg/logo.svg" class="logo" />';
-        
-        echo '</span>';
+            echo '<a href="' . $classes . '" class="link link--primary link--external action">' . 'Class schedules';
+          
+              set_query_var( 'icon', 'arrow-external' );
+              get_template_part('partials/icon');
+          
+            echo '</a>';
 
-      echo '</a>';
+          echo '</li>';
 
-      echo '<ul class="header__nav list">';
+          echo '<li class="">';
 
-        wp_nav_menu( $nav_primary_left );
+            echo '<button class="button button--primary">' . 'Book an appointment' . '</button>';  
+          
+          echo '</li>';
 
-      echo '</ul>';
+        echo '</ul>';
 
-    echo '</div>';
+      echo '</div>';
 
-    echo '<div class="header__group header__group--right">';
+      echo '<div class="header__group">';
 
-      echo '<ul class="header__nav list">';
+        echo '<a href="' . get_bloginfo('url') . '" class="header__logo">';
 
-        echo '<li class="">';
+          echo '<span class="visuallyhidden">' . get_bloginfo('name') . '</span>';
 
-          echo '<a href="' . $classes . '" class="link link--primary link--external action">' . 'Class schedules';
-        
-            set_query_var( 'icon', 'arrow-external' );
-            get_template_part('partials/icon');
-        
-          echo '</a>';
+          echo '<img src="' . get_bloginfo('stylesheet_directory') . '/assets/svg/logo.svg" />';
+          
+        echo '</a>';
 
-        echo '</li>';
+        echo '<ul class="header__nav list">';
 
-        echo '<li class="">';
+          wp_nav_menu( $nav_primary_left );
 
-          echo '<button class="button button--primary">' . 'Book an appointment' . '</button>';  
-        
-        echo '</li>';
+        echo '</ul>';
 
-      echo '</ul>';
-
+      echo '</div>';
+    
     echo '</div>';
   
-  echo '</div>';
+  echo '</nav>';
 
 echo '</header>';
 
