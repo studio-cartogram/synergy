f933c633b25573c49b1b45da02a375655bc93f12
feeohnah <fiona@studiocartogram.com>
2018-01-19 17:36:52 -0500

    services on services main page are just a list, no image

--- wp-content/themes/synergy-dev/theme/partials/item-services.php
+++ wp-content/themes/synergy-dev/theme/partials/item-services.php
@@ -7,21 +7,42 @@ $image = get_post_thumbnail_id();
 $hasimage = has_post_thumbnail();
 $size = 'large';
 
-if($hasimage) :
+$count = get_query_var('count');
+$total_count = get_query_var('total_count');
+
+if ( $count == 1 ): 
+
+echo '<div class="services__column -left">';
+
+elseif ( $count == round($total_count/3) ): 
+
+echo '</div>';
+
+echo '<div class="services__column -center">';
+
+elseif ( $count == round(($total_count/3)+($total_count/3)) ): 
+
+echo '</div>';
+
+echo '<div class="services__column -right">';
+
+endif;
 
   echo '<article id="' . $item->post_name . '" class="item item--services">';
 
     echo '<a class="' . ($context === 'single' ? ' is-active ' : '') . '" href="' . get_permalink($item->ID) . '">';
 
-      echo '<div class="item__image">';
+      // echo '<div class="item__image">';
 
-        echo wp_get_attachment_image( $image, $size );
+      //   echo wp_get_attachment_image( $image, $size );
 
-      echo '</div>';
+      // echo '</div>';
 
       echo '<div class="item__text">';
 
         echo '<h3 class="item__title">' . get_the_title($item->ID) . '</h3>';
+
+        // echo '<p class="item__title">' . get_the_excerpt($item->ID) . '</p>';
         
       echo '</div>';
 
@@ -29,6 +50,6 @@ if($hasimage) :
 
   echo '</article>';
 
-endif;
+
 
 ?>
