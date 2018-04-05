168ac2e30d589b238ca729b8c84b1427e7ffb6b2
feeohnah <fiona@studiocartogram.com>
2018-04-05 16:23:38 -0400

    made logo 20% bigger, fixed position of nav, brought back image and description for main services page

--- wp-content/themes/synergy/partials/item-services.php
+++ wp-content/themes/synergy/partials/item-services.php
@@ -10,46 +10,26 @@ $size = 'large';
 $count = get_query_var('count');
 $total_count = get_query_var('total_count');
 
-if ( $count == 1 ): 
+echo '<article id="' . $item->post_name . '" class="item item--services">';
 
-echo '<div class="services__column -left">';
+  echo '<a class="' . ($context === 'single' ? ' is-active ' : '') . '" href="' . get_permalink($item->ID) . '">';
 
-elseif ( $count == round($total_count/3) ): 
+    echo '<div class="item__image">';
 
-echo '</div>';
+      echo wp_get_attachment_image( $image, $size );
 
-echo '<div class="services__column -center">';
+    echo '</div>';
 
-elseif ( $count == round(($total_count/3)+($total_count/3)) ): 
+    echo '<div class="item__text">';
 
-echo '</div>';
+      echo '<h3 class="item__title">' . get_the_title($item->ID) . '</h3>';
 
-echo '<div class="services__column -right">';
+      echo '<p class="item__title secondary">' . get_the_excerpt($item->ID) . '</p>';
+      
+    echo '</div>';
 
-endif;
+  echo '</a>';
 
-  echo '<article id="' . $item->post_name . '" class="item item--services">';
-
-    echo '<a class="' . ($context === 'single' ? ' is-active ' : '') . '" href="' . get_permalink($item->ID) . '">';
-
-      // echo '<div class="item__image">';
-
-      //   echo wp_get_attachment_image( $image, $size );
-
-      // echo '</div>';
-
-      echo '<div class="item__text">';
-
-        echo '<h3 class="item__title">' . get_the_title($item->ID) . '</h3>';
-
-        // echo '<p class="item__title">' . get_the_excerpt($item->ID) . '</p>';
-        
-      echo '</div>';
-
-    echo '</a>';
-
-  echo '</article>';
-
-
+echo '</article>';
 
 ?>
